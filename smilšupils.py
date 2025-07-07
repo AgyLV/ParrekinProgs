fca5742b3d0e2f5e1473adefbeff7928f047f58f
AgyLV <70198664+AgyLV@users.noreply.github.com>
2025-07-07 17:21:19 +0300

    Add files via upload

---
+++ smilšupils.py
@@ -0,0 +1,20 @@
+import subprocess
+
+# Path to DOSBox executable
+dosbox_path = r"C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe"  # Update this to your DOSBox path
+
+# Commands to execute inside DOSBox
+commands = [
+    "mount c C:\\trans",
+    "c:",
+    "Riga_TR.exe Riga_TR.txt Riga_O.txt",
+    #"exit"  # To close DOSBox after running commands
+]
+
+# Construct the argument list
+args = [dosbox_path]
+for cmd in commands:
+    args.extend(["-c", cmd])
+
+# Run DOSBox with the commands
+subprocess.run(args)
